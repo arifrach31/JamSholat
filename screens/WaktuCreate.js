34a5b9792806a0903be33a07bc74c3413e44229b
arifrachcom <ardesain@Yahoo.com>
2018-03-03 23:11:05 +0700

    add form WaktuCreate.js

--- screens/WaktuCreate.js
+++ screens/WaktuCreate.js
@@ -2,16 +2,45 @@ import React, {Component} from 'react';
 import {
   Container,
   Content,
-  Text
+  Text,
+  Form,
+  Item,
+  Label,
+  Input,
+  Button
 } from 'native-base';
 
 export default class WaktuCreate extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleSubmit(){
+    alert(this.state.text1);
+    alert(this.state.text2);
+  }
 
   render(){
     return(
       <Container>
         <Content>
-          <Text>Tambah Waktu Baru</Text>
+          <Form>
+            <Item floatingLabel>
+              <Label>Nama Sholat</Label>
+              <Input onChangeText={(text1)=> this.setState({text1})}/>
+            </Item>
+            <Item floatingLabel>
+              <Label>Waktu Sholat</Label>
+              <Input onChangeText={(text2)=> this.setState({text2})}/>
+            </Item>
+            <Button block onPress={()=>this.handleSubmit()}>
+              <Text>Submit</Text>
+            </Button>
+          </Form>
         </Content>
       </Container>
     );
