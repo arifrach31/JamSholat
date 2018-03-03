093ae4cdeda58b885c34d69c8149a4d0ce9c480e
arifrachcom <ardesain@Yahoo.com>
2018-03-04 00:15:45 +0700

    Patch Data RestAPI

--- components/WaktuItem.js
+++ components/WaktuItem.js
@@ -8,15 +8,37 @@ import {
   Text
 } from 'native-base';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 export default class WaktuItem extends Component {
+
+  state = {
+    isDone: false
+  }
+
+  componentDidMount(){
+    const{sholat: {isDone}} = this.props;
+
+    this.setState({isDone: Boolean(isDone)});
+  }
+
+  handleDone(){
+    const{sholat: {id}} = this.props;
+    this.setState({
+      isDone: !this.state.isDone
+    });
+
+    axios.patch(`http://192.168.100.9:8000/api/jamsholat/${id}`, {
+      isDone: this.state.isDone? 0: 1
+    });
+  }
 
   render(){
     const {waktu: {id, namasholat, waktusholat}} = this.props;
     return (
       <ListItem key={id}>
         <Left>
-          <CheckBox checked={false}/>
+          <CheckBox checked={this.state.isDone} onPress={()=> this.handleDone()}/>
         </Left>
         <Body>
           <Text>{namasholat}</Text>
